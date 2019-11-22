832210baf77b884669fe6aab82b08bbb55507957
Mike Field-May <mikefieldmay@Mikes-MBP.home>
2019-11-22 16:18:22 +0000

    Update PR after master rebase

--- src/App.js
+++ src/App.js
@@ -1,32 +1,27 @@
-import React, { Component } from "react";
 import { Route } from "react-router-dom";
+import React, { Component } from "react";
 
 import { Users } from "./containers/Users";
 import { asyncComponent } from "./hoc/asyncComponent";
-import React, { Component } from 'react';
-import { Route } from 'react-router-dom';
-
-import { LinkButton } from './components/LinkButton/LinkButton';
-import { Users } from './containers/Users';
-import { asyncComponent } from './hoc/asyncComponent';
+import { LinkButton } from "./components/LinkButton/LinkButton";
 
 const AsyncPizza = asyncComponent(() => {
   return import("./containers/Pizza.js");
 });
 
 export class App extends Component {
-    render() {
-        return (
-            <div>
-                <div>
-                    <LinkButton to="/" title="Users" />
-                    <LinkButton to="/pizza" title="Pizza" />
-                </div>
-                <div>
-                    <Route path="/" exact component={Users} />
-                    <Route path="/pizza" component={AsyncPizza} />
-                </div>
-            </div>
-        )
-    }
+  render() {
+    return (
+      <div>
+        <div>
+          <LinkButton to="/" title="Users" />
+          <LinkButton to="/pizza" title="Pizza" />
+        </div>
+        <div>
+          <Route path="/" exact component={Users} />
+          <Route path="/pizza" component={AsyncPizza} />
+        </div>
+      </div>
+    );
+  }
 }
